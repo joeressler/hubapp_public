5ecef6353b3fcb9337ac565ef9b42ad9447dce8d
Joseph Ressler <joe.a.ressler@gmail.com>
2025-01-04 03:58:59 -0500

    Deployment fixes #1

--- Clean-Rack_sensor.sql
+++
@@ -1,9 +0,0 @@
-DELETE FROM AccuCart.dbo.Rack_sensor
-WHERE id>0;
-SELECT TOP (1000) [id]
-      ,[column]
-      ,[is_taken]
-      ,[timestamp]
-      ,[rack_capacity_id]
-      ,[feeder_id]
-  FROM [AccuCart].[dbo].[Rack_sensor]
